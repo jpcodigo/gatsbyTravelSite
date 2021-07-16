0b50d027b64ae1f85960d0f8b2d57a65a6e9ab61
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2021-07-16 13:33:26 -0500

    progress on header, installed react icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,38 @@
 import * as React from "react"
-import PropTypes from "prop-types"
+import { FaBars } from "react-icons/fa"
+
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 const Header = () => {
-  return <h1>Header</h1>
+  return (
+    <Nav>
+      <NavLink to="/">EXPLORIX</NavLink>
+      <Bars />
+    </Nav>
+  )
 }
 
 export default Header
+
+const Nav = styled.nav`
+  background: red;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  padding: 0.5rem calc((100vw - 1300px) / 2);
+  z-index: 100;
+  position: relative;
+`
+
+const NavLink = styled(Link)`
+  color: white;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+`
+
+const Bars = styled(FaBars)``
