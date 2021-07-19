aa320e609f398378be750f1233fecbf4d714572a
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2021-07-19 10:29:11 -0500

    finished trips component

--- src/components/Trips.tsx
+++ src/components/Trips.tsx
@@ -2,15 +2,17 @@ import React, { ReactElement } from "react"
 import styled from "styled-components"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
+import { Btn } from "./Btn"
+import { ImLocation } from "react-icons/im"
 
-const Trips = () => {
+const Trips = ({ heading }) => {
   const data: {} = useStaticQuery(graphql`
     query TripsQuery {
       allTripsJson {
         edges {
           node {
             alt
-            button
+            btn
             name
             img {
               childImageSharp {
@@ -29,9 +31,30 @@ const Trips = () => {
     const tripsArray: ReactElement[] = []
     data.allTripsJson.edges.forEach((item, idx) => {
       tripsArray.push(
-        <div key={idx}>
-          <Img fluid={item.node.img.childImageSharp.fluid} />
-        </div>
+        <ProductCard key={idx}>
+          <ProductImg
+            alt={item.node.alt}
+            fluid={item.node.img.childImageSharp.fluid}
+          />
+          <ProductInfo>
+            <TextWrap>
+              <ImLocation />
+              <ProductTitle>{item.node.name}</ProductTitle>
+            </TextWrap>
+            <Btn
+              to="/trips"
+              primary="true"
+              round="true"
+              css={`
+                position: absolute;
+                top: 420px;
+                font-size: 14px;
+              `}
+            >
+              {item.node.btn}
+            </Btn>
+          </ProductInfo>
+        </ProductCard>
       )
     })
     return tripsArray
@@ -39,7 +62,7 @@ const Trips = () => {
 
   return (
     <ProductsContainer>
-      <ProductsHeading>Heading</ProductsHeading>
+      <ProductsHeading>{heading}</ProductsHeading>
       <ProductsWrapper>{getTrips(data)}</ProductsWrapper>
     </ProductsContainer>
   )
@@ -61,4 +84,68 @@ const ProductsHeading = styled.div`
   color: black;
 `
 
-const ProductsWrapper = styled.div``
+const ProductsWrapper = styled.div`
+  padding: 0 2rem;
+
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 10px;
+  justify-items: center;
+
+  @media screen and (max-width: 1200px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media screen and (max-width: 868px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+const ProductCard = styled.div`
+  line-height: 2;
+  width: 100%;
+  height: 500px;
+  border-radius: 10px;
+  transition: 0.2s ease;
+
+  position: relative;
+`
+
+const ProductImg = styled(Img)`
+  height: 100%;
+  max-width: 100%;
+  border-radius: 10px;
+  filter: brightness(70%);
+  transition: 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+  position: relative;
+
+  &:hover {
+    filter: brightness(100%);
+  }
+`
+
+const ProductInfo = styled.div`
+  padding: 0 2rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media screen and (max-width: 280px) {
+    padding: 0 1rem;
+  }
+`
+
+const TextWrap = styled.div`
+  position: absolute;
+  top: 375px;
+
+  display: flex;
+  align-items: center;
+`
+
+const ProductTitle = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  margin-left: 0.5rem;
+`
