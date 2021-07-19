fcc167caddfe471cf27b38ef74ea6f109aed18b9
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2021-07-19 10:37:20 -0500

    created testimonials component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,14 @@ import Layout from "../components/Layout"
 import Seo from "../components/Seo"
 import Hero from "../components/Hero"
 import Trips from "../components/Trips"
+import Testimonials from "../components/Testimonials"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
     <Trips heading="Our Favorite Destinations" />
+    <Testimonials />
   </Layout>
 )
 
