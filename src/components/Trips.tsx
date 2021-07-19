14f73508553a750fb0d6edb6f20ba01f0aafed30
Jesse Priest <68927440+jpcodigo@users.noreply.github.com>
2021-07-19 00:57:49 -0500

    rendered images on page with gatsby-image

--- src/components/Trips.tsx
+++ src/components/Trips.tsx
@@ -1,11 +1,46 @@
-import React from "react"
+import React, { ReactElement } from "react"
 import styled from "styled-components"
+import Img from "gatsby-image"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Trips = () => {
+  const data: {} = useStaticQuery(graphql`
+    query TripsQuery {
+      allTripsJson {
+        edges {
+          node {
+            alt
+            button
+            name
+            img {
+              childImageSharp {
+                fluid(quality: 90) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  function getTrips(data) {
+    const tripsArray: ReactElement[] = []
+    data.allTripsJson.edges.forEach((item, idx) => {
+      tripsArray.push(
+        <div key={idx}>
+          <Img fluid={item.node.img.childImageSharp.fluid} />
+        </div>
+      )
+    })
+    return tripsArray
+  }
+
   return (
     <ProductsContainer>
       <ProductsHeading>Heading</ProductsHeading>
-      <ProductsWrapper>wrapper</ProductsWrapper>
+      <ProductsWrapper>{getTrips(data)}</ProductsWrapper>
     </ProductsContainer>
   )
 }
